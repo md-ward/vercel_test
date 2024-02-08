022212c08ed1638b149661c3a2c27bd3b35ae933
Mohamad Ward <hp16806@gmail.com>
2024-02-08 19:33:27 +0400

    added dashboard nav button to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-export default function Home() {
+import styles from "@/app/ui/home.module.css";
+import Link from "next/link";
+
+export default function Page() {
   return (
-    <main className="min-h-screen  aspect-auto bg-slate-300 ">
-      <section>
-        <h1 className="text-4xl mx-3">Home page </h1>
-        <div></div>
+    <main className="min-h-screen aspect-auto bg-slate-300">
+      <section className="flex flex-col transition-all duration-150 ease-in-out">
+        <h1 className="text-4xl mx-3">Home page</h1>
+        <div className="flex flex-col">
+          <h1 className={styles.warning}>This is just a testing for NEXT JS</h1>
+          <div className="flex flex-row px-8 bg-red-300 ">
+            <h1>To see the Dashboard, click on </h1>
+            <Link
+              className="ring-1 ml-2 hover:p-2 transition-all duration-150 ease-in-out hover:rounded-lg w-fit bg-white  rounded-sm px-2 "
+              href="/dashboard"
+            >
+              Dashboard
+            </Link>
+          </div>
+        </div>
       </section>
     </main>
   );
